c15e84a7f2a22de1a9975b7094392ec3d474eaae
Rizhiy <vasart169@gmail.com>
2017-08-22 00:47:44 +0300

    Add sudden death

--- puzzle.py
+++ puzzle.py
@@ -94,9 +94,14 @@ class GameGrid(Frame):
                 if game_state(self.matrix) == 'win':
                     self.grid_cells[1][1].configure(text="You", bg=BACKGROUND_COLOR_CELL_EMPTY)
                     self.grid_cells[1][2].configure(text="Win!", bg=BACKGROUND_COLOR_CELL_EMPTY)
+                    return 1
                 if game_state(self.matrix) == 'lose':
                     self.grid_cells[1][1].configure(text="You", bg=BACKGROUND_COLOR_CELL_EMPTY)
                     self.grid_cells[1][2].configure(text="Lose!", bg=BACKGROUND_COLOR_CELL_EMPTY)
+                    return -1
+            else:
+                return -1
+        return 0
 
     def generate_next(self):
         index = (self.gen(), self.gen())
